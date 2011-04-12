5efb02992653c8fbd93fb66a191d6cf5b187aa9a
Jordan Sissel <jls@semicomplete.com>
2011-04-12 16:02:07 -0700

    - allow following a file starting at a specific position (seek pos) - check for file deletion or truncation at EOF - factor a single close() method

--- lib/filewatch/tailglob.rb
+++ lib/filewatch/tailglob.rb
@@ -31,15 +31,19 @@ class FileWatch::TailGlob
       puts "Skipping follow on #{path}, File.file? == false"
     end
 
+    close(path) if @files.include?(path)
     @files[path] = File.new(path, "r")
     
     # TODO(sissel): Support 'since'-like support.
-    # Always start
     case seek
       when :end; @files[path].sysseek(0, IO::SEEK_END)
       when :beginning; # nothing
       else
-        # handle specific positions
+        if seek.is_a?(Numeric)
+          # 'seek' could be a number that is an offset from
+          # the start of the file. We should seek to that point.
+          @files[path].sysseek(seek, IO::SEEK_SET)
+        end # seek.is_a?(Numeric)
     end # case seek
   end # def follow_file
 
@@ -63,10 +67,12 @@ class FileWatch::TailGlob
   protected
   def file_action_modify(path, event, &block)
     loop do
+      file = @files[path]
       begin
-        data = @files[path].sysread(4096)
+        data = file.sysread(4096)
         yield event.name, data
       rescue EOFError
+        check_for_truncation_or_deletion(path, event, &block)
         #case @eof_actions[path]
           #when :reopen
             #puts "Reopening #{path} due to eof and new file"
@@ -79,11 +85,57 @@ class FileWatch::TailGlob
   end
 
   protected
+  def check_for_truncation_or_deletion(path, event, &block)
+    file = @files[path]
+    pos = file.sysseek(0, IO::SEEK_CUR)
+    #puts "EOF(#{path}), pos: #{pos}"
+
+    # Truncation is determined by comparing the current read position in the
+    # file against the size of the file. If the file shrank, than we should
+    # assume truncation and seek to the beginning.
+    begin
+      stat = file.stat
+      #p stat.size => pos
+      if stat.size < pos
+        # Truncated. Seek to beginning and read.
+        file.sysseek(0, IO::SEEK_SET)
+        file_action_modify(path, event, &block)
+      end
+    rescue Errno::ENOENT
+      # File was deleted or renamed. Stop following it.
+      close(path)
+    end
+  end # def follow_file
+
+  public
+  def subscribe(handler=nil, &block)
+    # TODO(sissel): Add handler support.
+    @watch.subscribe do |event|
+      path = event.name
+
+      event.actions.each do |action|
+        method = "file_action_#{action}".to_sym
+        if respond_to?(method)
+          send(method, path, event, &block)
+        else
+          $stderr.puts "Unsupported method #{self.class.name}##{method}"
+        end
+      end
+    end # @watch.subscribe
+  end # def subscribe
+
+  protected
   def reopen(path)
-    @files[path].close rescue nil
-    @files.delete(path)
+    close(path)
     follow_file(path, :beginning)
   end # def reopen
+
+  protected
+  def close(path)
+    @files[path].close rescue nil
+    @files.delete(path)
+    return nil
+  end
 
   protected
   def file_action_create(path, event, &block)
@@ -106,11 +158,14 @@ class FileWatch::TailGlob
   end # def file_action_create
 
   def file_action_delete(path, event, &block)
+    close(path)
     # ignore
   end
 
   def file_action_delete_self(path, event, &block)
-    p :delete_self => path
+    close(path)
+    # ignore
+    #p :delete_self => path
   end
 
   # Returns true if we are currently following the file at the given path.
