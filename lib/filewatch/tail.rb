f2f57da441200c39f05eae55c9e7b9b25fea99df
Pete Fritchman <petef@databits.net>
2011-08-30 02:17:29 -0700

    revamp/simplify interface

--- lib/filewatch/tail.rb
+++ lib/filewatch/tail.rb
@@ -1,92 +1,111 @@
-require "filewatch/namespace"
+require "filewatch/buftok"
 require "filewatch/watch"
+require "logger"
 
-class FileWatch::Tail
-  attr_accessor :logger
+module FileWatch
+  class Tail
+    attr_accessor :logger
 
-  public
-  def initialize
-    @watch = FileWatch::Watch.new
-    @files = {}
-    @logger = Logger.new(STDERR)
-  end # def initialize
+    public
+    def initialize(opts={})
+      @logger = Logger.new(STDERR)
+      @files = {}
+      @buffers = {}
+      @watch = FileWatch::Watch.new
+      @sincedb = {}  # TODO: load from disk
+      @statcache = {}
+    end # def initialize
 
-  public
-  def watch(path, opts={})
-    opts[:position] ||= IO::SEEK_END
-    if @files[path]
-      return # already watching
-    end
+    public
+    def logger=(logger)
+      @logger = logger
+    end # def logger=
 
-    # TODO(petef): add since-style support
-    if File.directory?(path)
-      @logger.warn("Skipping directory #{path}")
-      return
-    end
+    public
+    def tail(path)
+      @watch.watch(path)
+    end # def tail
 
-    @watch.watch(path, :create, :delete, :modify)
+    public
+    def subscribe(&block)
+      # subscribe(stat_interval = 1, discover_interval = 5, &block)
+      @watch.subscribe do |event, path|
+        case event
+        when :create, :create_initial
+          if @files.member?(path)
+            @logger.debug("#{event} for #{path}: already exists in @files")
+            next
+          end
+          _open_file(path, event)
+        when :modify
+          if !@files.member?(path)
+            @logger.debug(":modify for #{path}, does not exist in @files")
+            _open_file(path)
+          end
+          _read_file(path, &block)
+        when :delete
+          @logger.debug(":delete for #{path}, deleted from @files")
+          _read_file(path, &block)
+          @files[path].close
+          @files.delete(path)
+          @statcache.delete(path)
+        else
+          @logger.warn("unknown event type #{event} for #{path}")
+        end
+      end # @watch.subscribe
+    end # def each
 
-    if File.exists?(path)
-      file_action_create(path, opts)
-    end
-  end # def watch
-
-  def each(&block)
-    @watch.each do |path, event|
-      method = "file_action_#{event}".to_sym
-      if respond_to?(method)
-        send(method, path, &block)
-      end
-    end
-  end
-
-  def subscribe(&block)
-    loop do
-      each(&block)
-
-      sleep(1)
-    end
-  end # def subscribe
-
-  def file_action_create(path, opts={}, &block)
-    if @files[path]
-      raise FileWatch::Exception.new("#{path} got create but already open!")
-    end
-
-    opts[:position] ||= 0
-    begin
-      @files[path] = File.new(path, "r")
-      if opts[:position] == IO::SEEK_END
-        @files[path].sysseek(0, opts[:position])
-      else
-        @files[path].sysseek(opts[:position])
-      end
-    rescue Errno::EACCES
-      @logger.warn("Error opening #{path}: #{$!}")
-    end
-  end
-
-  def file_action_delete(path, &block)
-    file_action_modify(path, &block)  # read what we can from the FD
-
-    if @files[path]
-      @files[path].close
-      @files.delete(path)
-    end
-  end
-
-  def file_action_modify(path, &block)
-    file_action_create(path) unless @files[path]
-
-    loop do
+    private
+    def _open_file(path, event)
+      @logger.debug("_open_file: #{path}: opening")
+      # TODO(petef): handle File.open failing
       begin
-        data = @files[path].read_nonblock(4096)
-        yield path, data
-      rescue Errno::EWOULDBLOCK, Errno::EINTR
-        break
-      rescue EOFError
-        break
+        @files[path] = File.open(path)
+      rescue Errno::ENOENT
+        @logger.warn("#{path}: open: #{$!}")
+        @files.delete(path)
+        return
       end
-    end
-  end
-end # class FileWatch::Tail
+
+      stat = File::Stat.new(path)
+      inode = [stat.ino, stat.dev_major, stat.dev_minor]
+      @statcache[path] = inode
+
+      if @sincedb.member?(inode)
+        last_size = @sincedb[inode]
+        @logger.debug("#{path}: sincedb last value #{@sincedb[inode]}, cur size #{stat.size}")
+        if last_size <= stat.size
+          @logger.debug("#{path}: sincedb: seeking to #{last_size}")
+          @files[path].sysseek(last_size, IO::SEEK_SET)
+        else
+          @logger.debug("#{path}: last value size is greater than current value, starting over")
+          @sincedb[inode] = 0
+        end
+      elsif event == :create_initial && @files[path]
+        @logger.debug("#{path}: initial create, no sincedb, seeking to end #{stat.size}")
+        @files[path].sysseek(stat.size, IO::SEEK_SET)
+        @sincedb[inode] = stat.size
+      else
+        @logger.debug("#{path}: staying at position 0, no sincedb")
+      end
+    end # def _open_file
+
+    private
+    def _read_file(path, &block)
+      @buffers[path] ||= BufferedTokenizer.new
+
+      loop do
+        begin
+          data = @files[path].read_nonblock(4096)
+          @buffers[path].extract(data).each do |line|
+            yield(path, line)
+          end
+
+          @sincedb[@statcache[path]] = @files[path].pos
+        rescue Errno::EWOULDBLOCK, Errno::EINTR, EOFError
+          break
+        end
+      end
+    end # def _read_file
+  end # class Watch
+end # module FileWatch
--- lib/filewatch/watch.rb
+++ lib/filewatch/watch.rb
@@ -1,22 +1,122 @@
-require "filewatch/namespace"
-require "filewatch/stat"
-require "filewatch/exception"
+require "logger"
 
-class FileWatch::Watch
-  def initialize
-    @stat = FileWatch::Stat.new
-  end
+module FileWatch
+  class Watch
+    attr_accessor :logger
 
-  public
-  def watch(path, *what_to_watch)
-    return @stat.watch(path, *what_to_watch)
-  end # def watch
+    public
+    def initialize(opts={})
+      @logger = Logger.new(STDERR)
+      @watching = []
+      @files = Hash.new { |h, k| h[k] = Hash.new }
+    end # def initialize
 
-  def subscribe(handler=nil, &block)
-    @stat.subscribe(handler, &block)
-  end
+    public
+    def logger=(logger)
+      @logger = logger
+    end
 
-  def each(&block)
-    @stat.each(&block)
-  end # def each
-end # class FileWatch::Watch
+    # TODO(petef): support excludes
+
+    public
+    def watch(path)
+      if ! @watching.member?(path)
+        @watching << path
+        _discover_file(path, true)
+      end
+
+      return true
+    end # def tail
+
+    # Calls &block with params [event_type, path]
+    # event_type can be one of:
+    #   :create_initial - initially present file (so start at end for tail)
+    #   :create - file is created (new file after initial globs, start at 0)
+    #   :modify - file is modified (size increases)
+    #   :delete - file is deleted
+    public
+    def each(&block)
+      # Send any creates.
+      @files.keys.each do |path|
+        if ! @files[path][:create_sent]
+          if @files[path][:initial]
+            yield(:create_initial, path)
+          else
+            yield(:create, path)
+          end
+          @files[path][:create_sent] = true
+        end
+      end
+
+      @files.keys.each do |path|
+        begin
+          stat = File::Stat.new(path)
+        rescue Errno::ENOENT
+          # file has gone away or we can't read it anymore.
+          @files.delete(path)
+          @logger.debug("#{path}: stat failed (#{$!}), deleting from @files")
+          yield(:delete, path)
+          next
+        end
+
+        inode = [stat.ino, stat.dev_major, stat.dev_minor]
+        if inode != @files[path][:inode]
+          @logger.debug("#{path}: old inode was #{@files[path][:inode].inspect}, new is #{inode.inspect}")
+        elsif stat.size < @files[path][:size]
+          @logger.debug("#{path}: file rolled, new size is #{stat.size}, old size #{@files[path][:size]}")
+          yield(:delete, path)
+          yield(:create, path)
+        elsif stat.size > @files[path][:size]
+          @logger.debug("#{path}: file grew, old size #{@files[path][:size]}, new size #{stat.size}")
+          yield(:modify, path)
+        end
+
+        @files[path][:size] = stat.size
+        @files[path][:inode] = inode
+      end # @files.keys.each
+    end # def each
+
+    public
+    def discover
+      @watching.each do |path|
+        _discover_file(path)
+      end
+    end
+
+    public
+    def subscribe(stat_interval = 1, discover_interval = 5, &block)
+      glob = 0
+      loop do
+        each(&block)
+
+        glob += 1
+        if glob == discover_interval
+          discover
+          glob = 0
+        end
+
+        sleep(stat_interval)
+      end
+    end # def subscribe
+
+    private
+    def _discover_file(path, initial=false)
+      Dir.glob(path).each do |file|
+        next if @files.member?(file)
+        next unless File.file?(file)
+
+        @logger.debug("_discover_file: #{path}: new: #{file}")
+        stat = File::Stat.new(file)
+        @files[file] = {
+          :size => 0,
+          :inode => [stat.ino, stat.dev_major, stat.dev_minor],
+          :create_sent => false,
+        }
+        if initial
+          @files[file][:initial] = true
+        end
+      end
+    end # def _discover_file
+
+  end # class Watch
+end # module FileWatch
