ec10ccf743b8e22048b0a4f8204860d37346b158
Raj Sahae <rsahae@teslamotors.com>
2014-02-27 11:48:18 -0800

    Added comments

--- lib/filewatch/gzip.rb
+++ lib/filewatch/gzip.rb
@@ -7,14 +7,52 @@ require 'zlib'
 module FileWatch
   class Gzip < Tail
 
+    # Public: Initialize a new FileWatch::Gzip object
+    #
+    # opts - An options hash (default: {})
+    #        :sincedb_write_interval - The Integer in seconds write to database (default: 10)
+    #        :stat_interval - The Integer in seconds to sleep inbetween stat checks (default: 1)
+    #        :discover_interval - The Integer in seconds to wait between globs (default: 5)
+    #        :exclude - The array of Strings to exclude from glob check (default: [])
+    #
+    # Examples
+    #
+    #   FileWatch::Gzip.new({:discover_interval => 10, :exclude => ['/tmp/*']})
+    #
     def initialize(opts = {})
       opts[:start_new_files_at] = :beginning
       super(opts)
     end
+    
+    # Public: Watch a path for new gzipped files
+    #
+    # path - The String glob expression to watch for gzipped files
+    #
+    # Returns true if successfull, false otherwise
+    #
+    # Examples
+    #
+    #   gz = FileWatch::Gzip.new
+    #   gz.watch('/tmp/*gz') # => true
+    #
+    alias_method :watch, :tail
 
     public
-    ##
-    # yields |path, line| to block
+    # Public: Initiates the loop that watches for files. Takes a block.
+    #
+    # block - Mandatory block. It will be sent the path to a file and a line
+    #         from that file.
+    #
+    # Yields - The path of a file, and a line within that file
+    #
+    # Examples
+    #
+    #   gz = FileWatch::Gzip.new
+    #   gz.watch('/tmp/*gz')
+    #   gz.subscribe{|path, line| puts "#{path}:#{line}"}
+    #
+    # Returns nothing. Runs infinitely.
+    #
     def subscribe(&block)
       @watch.subscribe(@opts[:stat_interval],
                        @opts[:discover_interval]) do |event, path|
@@ -48,15 +86,29 @@ module FileWatch
     private
     def _read_file(path, &block)
       begin
+        changed = false
         @logger.debug("#{self.class}#_read_file"){"GzipReader on #{path}"}
         gz = Zlib::GzipReader.new(@files[path])
-        gz.each_line{|line| yield(path, line)}
+        gz.each_line do |line|
+          changed = true
+          yield(path, line)
+        end
+        @sincedb[@statcache[path]] = @files[path].pos
+
+        if changed
+          now = Time.now.to_i
+          delta = now - @sincedb_last_write
+          if delta >= @opts[:sincedb_write_interval]
+            @logger.debug("writing sincedb (delta since last write = #{delta})")
+            _sincedb_write
+            @sincedb_last_write = now
+          end
+        end
       rescue Zlib::Error, Zlib::GzipFile::Error, Zlib::GzipFile::NoFooter, Zlib::GzipFile::CRCError, Zlib::GzipFile::LengthError => e
         @logger.debug("#{self.class}#_read_file"){"#{e.class}:#{e.message} path:#{path}"}
       ensure
-        gz.close
+        gz.close unless gz.nil?
       end
-
     end # _read_file
 
   end # class Gzip
