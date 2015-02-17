6f0ec339f2cd2d4015fa842234db395c8283f037
Joao Duarte <jsvduarte@gmail.com>
2015-02-17 11:08:31 +0000

    spec style cleanup and test dependency tightened

--- Gemfile
+++ Gemfile
@@ -2,5 +2,5 @@ source "https://rubygems.org"
 
 group :test do
   gem 'rspec'
-  gem 'stud'
+  gem 'stud', "~> 0.0.18"
 end
--- spec/buftok_spec.rb
+++ spec/buftok_spec.rb
@@ -3,11 +3,8 @@ require 'filewatch/buftok'
 describe FileWatch::BufferedTokenizer do
 
   context "when using the default delimiter" do
-    subject { FileWatch::BufferedTokenizer.new }
-
     it "splits the lines correctly" do
-      message = "hello\nworld\n"
-      expect(subject.extract(message)).to eq ["hello", "world"]
+      expect(subject.extract("hello\nworld\n")).to eq ["hello", "world"]
     end
   end
 
@@ -15,8 +12,7 @@ describe FileWatch::BufferedTokenizer do
     subject { FileWatch::BufferedTokenizer.new("\r\n") }
 
     it "splits the lines correctly" do
-      message = "hello\r\nworld\r\n"
-      expect(subject.extract(message)).to eq ["hello", "world"]
+      expect(subject.extract("hello\r\nworld\r\n")).to eq ["hello", "world"]
     end
   end
 end
