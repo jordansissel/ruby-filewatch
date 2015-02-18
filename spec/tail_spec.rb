7576977abfd4f59553b1cb8d20955a93c8c39a61
wiibaa <philippeweber57@yahoo.fr>
2015-02-18 21:35:03 +0100

    adding testcase from @jsvd + close the created tempfile

--- spec/tail_spec.rb
+++ spec/tail_spec.rb
@@ -3,7 +3,7 @@ require 'stud/temporary'
 
 describe FileWatch::Tail do
 
-  let(:file_path) { Stud::Temporary.file.path }
+  let(:file_path) { f = Stud::Temporary.file ; f.close; f.path }
   let(:sincedb_path) { Stud::Temporary.file.path }
 
   before :each do
@@ -35,6 +35,21 @@ describe FileWatch::Tail do
 
     it "reads new lines off the file" do
       expect { |b| subject.subscribe(&b) }.to yield_successive_args([file_path, "line1"], [file_path, "line2"])
+    end
+  end
+
+  context "when a file is deleted" do
+    subject { FileWatch::Tail.new(:sincedb_path => sincedb_path, :start_new_files_at => :beginning) }
+
+    before :each do
+      File.open(file_path, "w") { |file|  file.write("line1\nline2\n") }
+      subject.tail(file_path)
+      File.unlink file_path
+    end
+
+    it "should not raise exception" do
+      Thread.new(subject) { sleep 0.1; subject.quit } # force the subscribe loop to exit
+      expect { subject.subscribe {|p,l| } }.to_not raise_exception
     end
   end
 end
